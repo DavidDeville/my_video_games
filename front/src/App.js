191a3b97ff9fe4e07a646589a50b0621c57c6aee
David <david.de-ville@epitech.eu>
2020-03-26 17:49:23 -0400

    fixed minor bugs, like being unable to go back to the login. User can now logout and his token is deleted

--- front/src/App.js
+++ front/src/App.js
@@ -8,19 +8,6 @@ import Login from "./Components/Login/Login.js";
 const App = () => {
   const [user, setUser] = useState(null);
 
-  /**
-   * userData contains the data inside the local storage
-   * If the userData is not null (user is logged), we restore the data
-   * of the userContext (accessible from all components)
-   */
-  // useEffect(() => {
-  //   const userData = JSON.parse(localStorage.getItem("token"));
-  //   console.log(userData);
-  //   if (userData != null) {
-  //     setUser(userData);
-  //   }
-  // }, [setUser]);
-
   useEffect(() => {
   }, []);
 
@@ -43,7 +30,7 @@ const App = () => {
             </Switch>
           ) : (
             <Switch>
-              <Route exact path="/" render={() => <Home />}></Route>
+              <Route exact path="/" render={() => <Home setUser={setUser} />}></Route>
             </Switch>
           )}
         </Router>
--- front/src/Components/Home/Home.js
+++ front/src/Components/Home/Home.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import game_icon from "../../Logos/game_icon.svg";
 import { useHistory } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
 import "./Home.css";
 
-const Home = ({ user }) => {
+const Home = ({ user, setUser }) => {
   const history = useHistory();
 
   const [loggedUser, setLoggedUser] = useState(null);
@@ -11,21 +12,40 @@ const Home = ({ user }) => {
   useEffect(() => {
     const userData = JSON.parse(localStorage.getItem("token"));
     setLoggedUser(userData);
-  }, [user, loggedUser]);
+  }, [user]);
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setLoggedUser(null);
+    setUser(null);
+  }
 
   return (
-    <div className="home_header">
+    <div className="home_main_bloc">
       {loggedUser !== null ? (
-        <div>
-          <p onClick={() => console.log(loggedUser)}>
-            Welcome {loggedUser.username} !
-          </p>
+        <div className="home_header">
+          <div>
+            <span className="home_title">GAMES COLLECTION</span>
+          </div>
+          <div>
+            <span className="home_login">Welcome {loggedUser.username} !</span>
+          </div>
+          <div>
+            <span onClick={() => logout()} className="home_logout">LOGOUT</span>
+          </div>
         </div>
       ) : (
-        <div>
+        <div className="options_header">
+            <div>
+                <></>
+            </div>
+            <div>
           <img src={game_icon} className="App-logo" alt="logo" />
-          <p onClick={() => history.push("/register")}>Register</p>
-          <p onClick={() => history.push("/login")}>Login</p>
+          </div>
+          <div>
+          <span className="register" onClick={() => history.push("/register")}>Register</span>
+          <span className="login" onClick={() => history.push("/login")}>Login</span>
+          </div>
         </div>
       )}
     </div>
